867ba69dfb6f22fa2eae412b59e176bcd37766ca
AleksandrKriuchkov <kriuchkovaleks88@gmail.com>
2022-07-03 15:43:12 +0400

    Final result

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿/*
 **Задача**: Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа. 
 Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
- При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
 **Примеры**:
 
@@ -36,9 +36,7 @@ int GetArrayLenght(string msg)
     }
 }
 
-
-
-string [] FillArray(int lenght)
+string[] FillArray(int lenght)
 {
     string[] arr = new string[lenght];
 
@@ -46,28 +44,56 @@ string [] FillArray(int lenght)
     {
         Console.WriteLine("Input array element");
         arr[i] = Console.ReadLine();
-
     }
-
-
     return arr;
 }
 
-void PrintArray(string [] array)
+void PrintArray(string[] array)
+
 {
     int length = array.Length;
     for (int i = 0; i < length; i++)
     {
-        Console.Write(array[i]);
+        Console.Write($"{array[i]} ");
     }
 }
+
+string[] GetResultArray(string[] array)
+{
+    int resultArrayLength = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 4)
+        {
+            resultArrayLength = resultArrayLength + 1;
+        }
+    }
+    string[] resultArray = new string[resultArrayLength];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 4)
+        {
+            resultArray[j] = array[i];
+            j++;
+        }
+    }
+    return resultArray;
+}
+
 string message = "Input array lenght";
 
 int ararayLenght = GetArrayLenght(message);
 
-string [] originalArray = FillArray(ararayLenght);
+string[] originalArray = FillArray(ararayLenght);
 
 PrintArray(originalArray);
 
+string[] result = GetResultArray(originalArray);
+
+Console.WriteLine();
+Console.WriteLine("RESULT ARRAY: ");
+PrintArray(result);
 
 
